32c704ddf4600fe19020a889bacfbbbb5eb94803
Manuel Buchfink <st173195@stud.uni-stuttgart.de>
2025-03-29 14:07:06 +0100

    detached shot transform from player after shooting

--- shot.gd
+++ shot.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var speed = 750
+@export var speed = 3500
 
 func _physics_process(delta):
 	position += transform.x * speed * delta
--- zombie_survival_shooter.gd
+++ zombie_survival_shooter.gd
@@ -8,16 +8,16 @@ extends Node2D
 func _ready() -> void:
 	for i in 10:
 		var new_zombie = zombie_scene.instantiate()
-	
+				
+		var random_height = randi_range(20 ,600)
+		var random_distance = randi_range(900 ,1300)
+		var random_velocity = randi_range(30,100)
 		
-		var random_height = randf_range(5 ,630)
-		var random_distance = randf_range(900 ,1300)
 		new_zombie.position = Vector2(random_distance, random_height)
+		new_zombie.speed = random_velocity
 		
 		add_child(new_zombie)
 	
-	
-	#pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
